941269cd3c6c8d9528be6ac9bfec69bf651face3
Martin Favin-Lévêque <martin.fl97@gmail.com>
2025-01-06 11:37:24 +0100

    Remove scope matching from matchers

--- matchers.py
+++ matchers.py
@@ -41,85 +41,23 @@ ann_assign_matcher = m.AnnAssign(
     target=assign_name, annotation=assign_type, value=assign_value
 )
 
-GLOBAL_SCOPE_NAME = ""
+statement_matcher = m.SimpleStatementLine(
+    body=[m.OneOf(assign_matcher, ann_assign_matcher)],
+    trailing_whitespace=m.OneOf(
+        m.TrailingWhitespace(comment=comment),
+        m.TrailingWhitespace(comment=no_comment),
+    ),
+)
 
-
-class UnnamedScopeError(ValueError):
-    pass
-
-
-def resolve_scope_name(scope: metadata.Scope) -> str:
-    """Resolve scope path as a dot-separated chain of child scope names.
+def _test():
+    import libcst as cst
+    from pathlib import Path
+    from node_converter import resolve_scope_name
+    code = cst.parse_module(Path("test_data/test_script.py").read_text())
+    wrapper = metadata.MetadataWrapper(code)
+    matches = m.extractall(wrapper, statement_matcher)
+    print([type(m["scope"]) for m in matches])
     
-    This function differs from what `libcst.metadata.QualifiedNameProvider` can provide \
-        since it omits `<local>` scopes, and raises an exception when 
     
-    Example:
-        While analyzing the scope metadata of following module:
-        ```python
-        class Foo:
-            def bar(self):
-                def baz():
-                    bat = 5
-        ```
-        resolving the scope name of `baz` returns `"Foo.bar.baz"`
-    
-    Args:
-        scope (metadata.Scope): The libcst.metadata.Scope object to be resolved.
-        
-    Raises:
-        UnnamedScopeError: The provided scope or one of its parent scopes is not named, e.g. `lambda` function scopes.
-
-    Returns:
-        str: A dot-separated string chaining all scope names up from the global scope. 
-    """
-
-    def _resolve_scope_name(scope: metadata.Scope, children: str = "") -> str:
-        if isinstance(scope, metadata.GlobalScope):
-            return children
-        elif getattr(scope, "name", None) is None:
-            raise UnnamedScopeError(
-                f"Cannot resolve name for unnamed scope: '{scope._name_prefix}'"
-            )
-        else:
-            if not children:
-                new_children = scope.name
-            else:
-                new_children = f"{scope.name}.{children}"
-            return _resolve_scope_name(scope.parent, new_children)
-
-    return _resolve_scope_name(scope)
-
-
-def build_scoped_statement_matcher(scope_name: str) -> m.SimpleStatementLine:
-    """Generate a matcher that extracts information from assignements in a given scope.
-
-    Args:
-        scope_name (str): Dot-separated path of the scope to inspect, like `"Foo.bar.baz"`. Set to `""` for inspecting global scope.
-
-    Returns:
-        m.SimpleStatementLine: A `libcst.matchers.Matcher` object matching the first tima a variable is assigned in a scope, \
-            and extracting its name, the assigned value, the type annotation and the trailing comment.  
-    """
-
-    def scope_matcher(scope: metadata.Scope):
-        try:
-            return resolve_scope_name(scope) == scope_name
-        except UnnamedScopeError:
-            return False
-
-    return m.SimpleStatementLine(
-        metadata=m.MatchMetadataIfTrue(
-            metadata.ScopeProvider,
-            scope_matcher,
-        ),
-        body=[m.OneOf(assign_matcher, ann_assign_matcher)],
-        trailing_whitespace=m.OneOf(
-            m.TrailingWhitespace(comment=comment),
-            m.TrailingWhitespace(comment=no_comment),
-        ),
-    )
-
-
-# Match a global statement containing an assignment
-global_statement_matcher = build_scoped_statement_matcher(GLOBAL_SCOPE_NAME)
+if __name__ == "__main__":
+    _test()
